ebf7b6e61b88c9ae6d867d05a916f04288e9e76f
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-16 21:02:51 +0300

    Styles the site

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ function handleClick(ramen) {
     document.getElementById("action-buttons").style.display = "block";
 }
 
+document.getElementById("action-buttons").style.display = "none";
 
+document.getElementById("action-buttons").style.display = "none";
 //Function to handle form submission
 
 function addSubmitListener() {
@@ -84,6 +86,56 @@ function addSubmitListener() {
     });
 }
 
+//update function
+function addupdateListener() {
+    const ratingInput = document.getElementById("ramen-rating");
+    const commentInput = document.getElementById("ramen-comment");
+
+    ratingInput.addEventListener("input", () => {
+        if (selectedRamen) {
+            selectedRamen.rating = ratingInput.value;
+        }
+    });
+
+    commentInput.addEventListener("input", () => {
+        if (selectedRamen) {
+            selectedRamen.rating = ratingInput.value;
+        }
+    });
+
+    commentInput.addEventListener("input", () => {
+        if (selectedRamen) {
+            selectedRamen.comment = commentInput.value;
+        }
+    });
+}
+
+// delete function
+
+function addDeleteListener() {
+    const deleteButton = document.getElementById("delete-ramen");
+
+    deleteButton.addEventListener("click", () => {
+        if (selectedRamen) {
+            const index = ramens.findIndex((r) => r.id === selectedRamen.id);
+            if (index !== -1) {
+                ramens.splice(index, 1); //removes ramen from array
+            }
+
+            const ramenMenu = document.getElementById("ramen-menu");
+            ramenMenu.innerHTML = "";
+            displayRamens(); //removes image from menu and reloads the remaining images
+
+            //resets details
+            document.getElementById("ramen-image").src = "";
+            document.getElementById("ramen-name").textContent = "";
+            document.getElementById("ramen-restaurant").textContent = "";
+            document.getElementById("ramen-rating").value = "";
+            document.getElementById("ramen-comment").textContent = "";
+        }
+    });
+}
+
 
     
 
@@ -92,4 +144,6 @@ function addSubmitListener() {
 function main() {
     displayRamens();
     addSubmitListener();
+    addupdateListener();
+    addDeleteListener();
 }
